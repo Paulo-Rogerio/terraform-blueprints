c262db930ca96e378a7d7c618df15b3690db7ed6
Paulo Rogerio <paulo.silva@opendatacenter.com.br>
2025-05-06 08:39:07 -0300

    teste

--- cloudstack/vpc/simple/output.tf
+++ cloudstack/vpc/simple/output.tf
@@ -16,7 +16,7 @@ output "vpc_display_text" {
   value       = module.vpc.vpc_display_text
 }
 
-output "vpc_network_ids" {
-  description = "The IDs of the network/tier in the VPC"
-  value       = module.vpc.network_id
-}
+#output "vpc_network_ids" {
+#  description = "The IDs of the network/tier in the VPC"
+#  value       = module.vpc.network_id
+#}

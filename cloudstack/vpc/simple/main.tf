f6f1d6c99dbdd884631208260a07aa109ce78d7f
Paulo Rogerio <paulo.silva@opendatacenter.com.br>
2025-05-06 10:29:41 -0300

    Correcoes

--- cloudstack/vpc/simple/main.tf
+++ cloudstack/vpc/simple/main.tf
@@ -6,12 +6,11 @@
 module "vpc" {
   source = "git::https://github.com/Paulo-Rogerio/terraform-modules.git//cloudstack/vpc?ref=v0.0.5"
 
-  #source = "../../../../terraform-modules/cloudstack/vpc"
 
   name                     = "simple-example"
   cidr                     = "10.0.0.0/16"
   zone                     = "Enterprise_Internet_Bi"
-  vpc_offering             = "Default VPC offering"
+  vpc_offering             = "Paulera VPC offering"
   subnets                  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
   default_network_offering = "NAT for VPC"
   subnet_names             = ["nw-1", "nw-2", "nw-3"]

50f13937992eb3bff3c979fd8e66d5347adef3ac
Paulo Rogério Gomes e Silva <psilva.gomes.rogerio@gmail.com>
2025-05-06 23:21:22 -0300

    correcoes

--- cloudstack/vpc/simple/main.tf
+++ cloudstack/vpc/simple/main.tf
@@ -7,15 +7,23 @@ module "vpc" {
   source = "git::https://github.com/Paulo-Rogerio/terraform-modules.git//cloudstack/vpc?ref=v0.0.5"
 
   name                     = var.name
-  cidr                     = "10.0.0.0/16"
-  zone                     = "Enterprise_Internet_Bi"
-  vpc_offering             = "Paulera VPC offering"
-  subnets                  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-  default_network_offering = "NAT for VPC"
-  subnet_names             = ["nw-1", "nw-2", "nw-3"]
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  cidr                     = var.cidr
+  zone                     = var.zone
+  vpc_offering             = var.vpc_offering
+  subnets                  = var.subnets
+  default_network_offering = var.default_network_offering
+  subnet_names             = var.subnet_names
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags
+  )
+
+  #tags = {
+  #  Terraform   = "true"
+  #  Environment = "dev"
+  #}
 }
 

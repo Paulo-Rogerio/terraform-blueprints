56f6948184961b6aaebb51192dd92fb1a2a8fd35
Paulo Rogério Gomes e Silva <psilva.gomes.rogerio@gmail.com>
2025-05-27 07:15:53 -0300

    correcoes

--- cloudstack/vpc/simple/main.tf
+++ cloudstack/vpc/simple/main.tf
@@ -4,15 +4,17 @@
 # ------------------------------------------------------------------------------
 
 module "vpc" {
-  source = "git::https://github.com/Paulo-Rogerio/terraform-modules.git//cloudstack/vpc?ref=v0.0.5"
+  #source = "git::https://github.com/Paulo-Rogerio/terraform-modules.git//cloudstack/vpc?ref=v0.0.6s"
+
+  source                   = "../../../terraform-modules/cloudstack/vpc"
 
   name                     = var.name
   cidr                     = var.cidr
   zone                     = var.zone
   vpc_offering             = var.vpc_offering
-  subnets                  = var.subnets
-  default_network_offering = var.default_network_offering
-  subnet_names             = var.subnet_names
+  #subnets                  = var.subnets
+  #default_network_offering = var.default_network_offering
+  #ssubnet_names             = var.subnet_names
 
   #tags = merge(
   #  {
